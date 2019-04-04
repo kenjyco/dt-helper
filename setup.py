380ef27780d296ffb4b6294b843e7354793c890e
Ken <kenjyco@gmail.com>
2019-04-04 06:55:07 -0500

    Bump to v0.0.6

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open('README.rst', 'r') as fp:
 
 setup(
     name='dt-helper',
-    version='0.0.5',
+    version='0.0.6',
     description='Helper funcs for doing things with datetime objects and UTC floats',
     long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/dt-helper',
-    download_url='https://github.com/kenjyco/dt-helper/tarball/v0.0.5',
+    download_url='https://github.com/kenjyco/dt-helper/tarball/v0.0.6',
     packages=find_packages(),
     install_requires=[
         'pytz',
