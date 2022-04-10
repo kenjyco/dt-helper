7181850a12a376ecf5b4abe7ecdcb2d60797f3c9
Ken <kenjyco@gmail.com>
2022-04-10 15:28:53 -0500

    Update keywords and classifiers in setup.py

--- setup.py
+++ setup.py
@@ -27,11 +27,14 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Software Development :: Libraries',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
     ],
-    keywords = ['datetime', 'date', 'time', 'utc', 'helper']
+    keywords=['datetime', 'date', 'time', 'utc', 'helper', 'kenjyco']
 )
